26c85de467cb56bda908da666accbcecd9c9a0d8
Ömer Turan ÖZTOKMAK <o.t.oztokmak@gmail.com>
2022-10-17 01:01:30 +0300

    PatikaBootcamp|Employee|JAVA
    By Ömer Turan ÖZTOKMAK

---
+++ Employee Project Bootcamp/src/Main.java
@@ -0,0 +1,7 @@
+
+public class Main {
+	   public static void main(String[] args) {
+	        Employee employee = new Employee("George AMBERSON", 13000, 60, 3 );
+	        employee.toString();
+	    }
+}
